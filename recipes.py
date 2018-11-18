ab8cda46706dafd960fc955cea55ff6570632589
Luca DEttorre <ldettorre21@gmail.com>
2018-11-18 20:47:56 +0000

    Uploaded Bootstrap CSS, Bootstrap JS and Jquery to the app, set basic file structure and have it linked to the base.html page correctly

--- recipes.py
+++ recipes.py
@@ -27,4 +27,4 @@ def get_recipes():
 
 
 if __name__ == '__main__':
-    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
+    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)),debug=True)
