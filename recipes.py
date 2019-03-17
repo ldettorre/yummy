a00d97e10c55915d166789854b6bcb2de20a9145
Luca DEttorre <ldettorre21@gmail.com>
2019-03-17 18:37:28 +0000

    Pip uninstalled flask_bcrypt and fixed the layout issue for listing authors on mobile devices such as smart phones

--- recipes.py
+++ recipes.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask, render_template, redirect, request, url_for, session, flash
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-from flask_bcrypt import bcrypt
 
 
 
